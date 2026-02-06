6599c466a1c97fa328dab2d84248f8009ca46ac2
vaishnavi chavan <vaishnavichavan653@gmail.com>
2026-02-06 12:43:11 +0530

    Add files via upload

---
+++ Employee.java
@@ -0,0 +1,47 @@
+public class Employee {
+    public static void main(String[]args){
+    private String firstName;
+    private String lastName;
+    private double monthlySalary;
+    public Employee(String firstName, String lastName, double monthlySalary) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+
+        if (monthlySalary > 0)
+            this.monthlySalary = monthlySalary;
+        else
+            this.monthlySalary = 0.0;
+    }
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+   
+    public void setMonthlySalary(double monthlySalary) {
+        if (monthlySalary > 0)
+            this.monthlySalary = monthlySalary;
+        else
+            this.monthlySalary = 0.0;
+    }
+
+    public double getMonthlySalary() {
+        return monthlySalary;
+    }
+    public double getYearlySalary() {
+        return monthlySalary * 12;
+    }
+}
+}
+
+
