2b403b18aa477b57672574d2cc022d6f029ec620
vaishnavi chavan <vaishnavichavan653@gmail.com>
2026-02-06 10:05:03 +0530

    Add files via upload

---
+++ Factorial.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Factorial{
+    public static void main(String[] abc){
+        int n,fact=1;
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter number:");
+        n=sc.nextInt();
+        for(int i=1;i<=n;i++){
+            fact=fact*i;
+
+        }
+        System.out.println("Factorial of"+ n +"is:"+fact);
+
+    }
+}
