2fcfd0de26f1d6d560040ceff8b40b0a9a543bb9
vaishnavi chavan <vaishnavichavan653@gmail.com>
2026-02-06 12:44:06 +0530

    Add files via upload

--- Sort.java
+++ Sort.java
@@ -17,3 +17,60 @@ class Sort{
         }
     }
 }
+
+
+
+
+
+public class Employee {
+    // Instance variables
+    private String firstName;
+    private String lastName;
+    private double monthlySalary;
+
+    // Constructor
+    public Employee(String firstName, String lastName, double monthlySalary) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+
+        if (monthlySalary > 0)
+            this.monthlySalary = monthlySalary;
+        else
+            this.monthlySalary = 0.0;
+    }
+
+    // Setter and Getter for First Name
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    // Setter and Getter for Last Name
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    // Setter and Getter for Monthly Salary
+    public void setMonthlySalary(double monthlySalary) {
+        if (monthlySalary > 0)
+            this.monthlySalary = monthlySalary;
+        else
+            this.monthlySalary = 0.0;
+    }
+
+    public double getMonthlySalary() {
+        return monthlySalary;
+    }
+
+    // Method to calculate yearly salary
+    public double getYearlySalary() {
+        return monthlySalary * 12;
+    }
+}
